d789e4b4d913f9471721966fc3638301a19d86ef
Ishasavani1402 <123747477+Ishasavani1402@users.noreply.github.com>
2024-01-13 22:14:06 +0530

    Add files via upload

---
+++ binarytree.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct tree
+{
+    int info;
+    struct tree *left, *right;
+};
+
+// create tree node
+struct tree *createtreenode(int info)
+{
+    struct tree *root = (struct tree *)malloc(sizeof(struct tree));
+    root->info = info;
+    root->left = NULL;
+    root->right = NULL;
+    return root;
+}
+
+// preorder traversal
+void preorder(struct tree *root)
+{
+    if (root != NULL)
+    {
+        printf("\n elements-%d", root->info);
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+
+void postorder(struct tree *root)
+{
+    if (root != NULL)
+    {
+        postorder(root->left);
+        postorder(root->right);
+        printf("\n elements=%d", root->info);
+    }
+}
+
+void inorder(struct tree *root)
+{
+    if(root!=NULL)
+    {
+        inorder(root->left);
+        printf("\n elements-%d",root->info);
+        inorder(root->right);
+    }
+}
+int main()
+{
+    struct tree *root = createtreenode(10);
+    struct tree *left = createtreenode(20);
+    struct tree *right = createtreenode(30);
+
+    root->left = left;
+    root->right = right;
+    left->left = createtreenode(40);
+    left->right = createtreenode(50);
+    right->left = createtreenode(60);
+    right->right = createtreenode(80);
+    printf("\n Preorder Traversal: ");
+    preorder(root);
+    printf("\n Postorder Traversal:");
+    postorder(root);
+    printf("\n inorder traversal");
+    inorder(root);
+
+    free(root->left->left);
+    free(root->left->right);
+    free(root->right->left);
+    free(root->right->right);
+    free(left);
+    free(right);
+    free(root);
+    return 0;
+}
+
+// struct tree *root = (struct tree *)malloc(sizeof(struct tree));
+// printf("Enter the root value: ");
+// scanf("%d", &root->info);
+// root->left = NULL;
+// root->right = NULL;
+
+// struct tree *leftnode = (struct tree *)malloc(sizeof(struct tree));
+// printf("\nEnter the left subtree's value: ");
+// scanf("%d", &leftnode->info);
+// leftnode->left = NULL;
+// leftnode->right = NULL;
+
+// struct tree *rightnode = (struct tree *)malloc(sizeof(struct tree));
+// printf("Enter the right subtree's value: ");
+// scanf("%d",&rightnode->info);
+// rightnode->left = NULL;
+// rightnode->right = NULL;
+
+// root->left = leftnode;
+// root->right = rightnode;
